a5dc91cacf6dfa982d9a7dd869eda82418e3dbc8
melanyvlm <mel.valencia28@outlook.com>
2019-03-30 17:00:05 -0500

    initipage

--- src/temp/initialpage.js
+++ src/temp/initialpage.js
@@ -3,42 +3,60 @@
 let initPage = {
   render: async() => {
     let view = `
-    <section class= "container-init" >
-    <div class="img-ppl">
-    <img src="imagenes/cerebron.png" alt="" class="center-img">
+    <section class= "template-register rps-design-form" >
+    <img src="imagenes/logo.png" alt="" class="playLogo">
+     <form class="auth">
+     <div class="container">
+     <div class="gray-div">
      </div>
-    <form class= "form-init" >
-    <div class="btn-div btn-bottom">
-      <a href="#/post" id="button-to-create-new-user" type="text/html"
-      class= "button center-text green">DEMUÉSTRALO! </a>
-    </div>
-    </form>
-    </section>
-    <div id="slide" >
-    <img src="Css/img/jiko.jpg" alt="imagen referencial"/>
-    </div>
-    <div class="template-rps slid">
-    <form class= "form-init" >
-    <img  src="Css/img/medium-logo-copia.png" class=" logo" >
-    <h1>Empodérate Hoy! </h1>
-    <h2>Únete a la red social de mujeres más grande de Lationoamérica! </h2>
-    <div class="btn-rps">
-    <a href=""  id="btn-register" class=" btn-init  btns-beside front-btn">
-    ¿Nuevo usuario?
-    </a>
-    <a href="" id="btn-log-in" class="btn-init btns-beside front-btn">
-      Iniciar sesión
-    </a>
-    </div>
-    </form>
-    </div>
+       <div class= "btn-div">
+       <p class="type-letter">CEREBRÓN</p>
+       <a href="#/Post" id="button-to-create-new-user" type="text/html"
+       class= "button center-text green showme">DEMUÉSTRALO ! </a>
+          </div>
+     </div>
+     </form>
+     </section> 
+
   `;
     return view;
   },
   afterRender: async() => {}
 };
-
 export default initPage;
-{/* <a href="#/Login" id="btn-log-in" class="btn-init btns-beside front-btn">
-Iniciar sesión
-</a>   */}
+/* <section class= "template-register rps-design-form" >
+<img src="imagenes/logo.png" alt="" class="playLogo">
+ <form class="auth">
+ <div class="container">
+ <div class="gray-div">
+ </div>
+   <div class= "btn-div">
+   <p class="type-letter">CEREBRÓN</p>
+   <p> ¿Sabía</p>
+
+ </div>
+ </div>
+ </form>
+ </section> 
+
+    // <a id="btn-Sign-Out" type="text/html" class= "button rubik-font gray-back black center-text">Cerrar sesión</a>
+    // export default initPage; */
+
+/* <section class= "template-register rps-design-form" >
+<img src="imagenes/cerebron.png" alt="" class="playLogo">
+<form class="auth">
+<div class="container">
+<div class="gray-div">
+<h2 class= "center-text rubik-font">¿Sabes más que yo?</h2>
+</div>
+  <div class= "btn-div">
+  <a href="#/Post" id="button-to-create-new-user" type="text/html"
+  class= "button center-text green">DEMUÉSTRALO ! </a>
+</div>
+</div>
+</form>
+</section> */
+
+/* <div class="img-ppl">
+<img src="imagenes/frame.png" alt="Logo purpura en forma de ola, simbolo de evolución, equidad y transformación " class="set-back mySlides">
+</div> */
